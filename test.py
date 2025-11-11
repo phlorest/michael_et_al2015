6d49e6145cecba19a20bf1e22e9643e83b3044e5
Robert Forkel <xrotwang@googlemail.com>
2025-11-11 09:57:10 +0100

    release v1.1

--- test.py
+++ test.py
@@ -5,4 +5,3 @@ def test_valid(cldf_dataset, cldf_logger):
 def test_phlorest_check(cldf_dataset, cldf_logger):
     from phlorest.check import run_checks
     assert run_checks(cldf_dataset, cldf_logger)
-
